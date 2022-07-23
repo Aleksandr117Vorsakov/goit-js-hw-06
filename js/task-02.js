bba4e8f2bb1954b36d8cef3311dd675205edb255
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-07-23 16:00:18 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEm = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((str, item) => str + `<li class="item">${item}</li>`, '');
+
+ulEm.innerHTML = list ;
+
+
+// const ulEl = document.querySelector('#ingredients');
+
+// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+// ulEl.innerHTML = list;
