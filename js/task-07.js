5ce947147536938a09cace757c16c54af5264600
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-07-28 19:42:01 +0300

    task-7

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+
+const inpEl = document.querySelector("#font-size-control");
+const outputSize = document.querySelector("#text");
+
+// outputSize.classList.add(input-style);
+
+inpEl.addEventListener("input", (element) => {
+  const valInput = element.target.value;
+  outputSize.style.fontSize =`${valInput}px`;
+  return outputSize.style.fontSize;
+});
